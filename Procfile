11d40a8254d3c3bc2b2c82d5adb55a0514dfa471
bzquatre <bzquatre@gmail.com>
2022-07-28 12:10:35 +0100

    update python vertion

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web website.wsgi:apllication --log-file -
+web: gunicorn CCMS.wsgi --log-file -
--- website/settings.py
+++ website/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)yl5ixlhp(epjkku)_d5&ud4_66wa_ka5orooj4!9h^7#b%n#2'
+SECRET_KEY = 'asqfsf54-insecure-)yl5ixlhp(epjkku)_d5&ud4_66wa_ka5orooj4!9h^7#b%n#2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','k-power.ml']
 
